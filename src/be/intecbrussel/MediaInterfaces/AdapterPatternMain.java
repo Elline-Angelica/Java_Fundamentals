c4ad0baf8ffc3eafabd6cf1690c6c7516b2d487c
Apple <ellinemedrano1408@hotmail.com>
2020-11-17 11:48:52 +0100

    Updated

--- src/be/intecbrussel/MediaInterfaces/AdapterPatternMain.java
+++ src/be/intecbrussel/MediaInterfaces/AdapterPatternMain.java
@@ -8,5 +8,12 @@ public class AdapterPatternMain {
         audioPlayer.play("vlc", "34+35.vlc");
         audioPlayer.play("avi", "pov");
 
+        //output - Ariana's songs
+        // Playing mp3 file. Name: my hair.mp3
+        //Playing mp4 file. Name: positions.mp4
+        //Playing vlc file. Name: 34+35.vlc
+        //Invalid media. avi format not supported
+
+
     }
 }
