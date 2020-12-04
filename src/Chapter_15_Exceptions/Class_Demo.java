22ce2452b88bc93115329fa70155ff3bea8434bf
Apple <ellinemedrano1408@hotmail.com>
2020-12-04 10:40:29 +0100

    Updated

---
+++ src/Chapter_15_Exceptions/Class_Demo.java
@@ -0,0 +1,12 @@
+package Chapter_15_Exceptions;
+
+public class Class_Demo {
+
+    public static void main(String[] args) {
+        System.out.println(2/0);
+        //System.out.println(args[0] = "hello");
+        //System.out.println(new int[]{1,2,3}[3]);
+
+    }
+
+}
