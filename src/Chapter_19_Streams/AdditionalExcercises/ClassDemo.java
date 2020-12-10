8a4c6cd317d5f43a3abc17ccab8a25138078152f
Apple <ellinemedrano1408@hotmail.com>
2020-12-10 10:36:53 +0100

    Updated

---
+++ src/Chapter_19_Streams/AdditionalExcercises/ClassDemo.java
@@ -0,0 +1,12 @@
+package Chapter_19_Streams.AdditionalExcercises;
+
+public class ClassDemo {
+    public static void main(String[] args) {
+
+    }
+
+    public static void method1(){
+
+    }
+
+}
---
+++ src/Chapter_19_Streams/P310/Excercise1.java
@@ -0,0 +1,11 @@
+package Chapter_19_Streams.P310;
+
+public class Excercise1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void method1(){
+
+    }
+}
