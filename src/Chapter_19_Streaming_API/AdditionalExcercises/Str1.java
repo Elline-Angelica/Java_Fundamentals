5aff444ea3e60d4e6e7e31e8cb18e7cedd21ea53
Apple <ellinemedrano1408@hotmail.com>
2021-01-03 16:30:44 +0100

    Updated

--- src/Chapter_19_Streaming_API/AdditionalExcercises/Str1.java
+++ src/Chapter_19_Streaming_API/AdditionalExcercises/Str1.java
@@ -1,6 +1,8 @@
 package Chapter_19_Streaming_API.AdditionalExcercises;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Str1 {
     public static void main(String[] args) {
@@ -8,14 +10,14 @@ public class Str1 {
         //1. Integer Stream
         IntStream
                 .range(1,10)
-                .forEach(System.out::print);
+                .forEach(System.out::println);
         System.out.println();
 
         //2. Integer Stream with skip
         IntStream
                 .range(1,10)
                 .skip(5)
-                .forEach(System.out::print);
+                .forEach(System.out::println);
         System.out.println();
 
         //3. Integer Stream with sum
@@ -25,6 +27,17 @@ public class Str1 {
                 .sum());
         System.out.println();
 
-        //4. Stream
+        //4. Stream .of, sorted and findFirst
+        Stream.of("Ava", "Adam", "Flynn")
+                .sorted()
+                .findFirst()
+                .ifPresent(System.out::println);
+
+        //5. Stream from Array, sort, filter and print
+        String[] names = {"Al","Beta","Gamma","Echo","Adam","Sofia","Selena"};
+        Arrays.stream(names) //Stream.of(names)
+                .filter( x -> x.startsWith("S"))
+                .sorted()
+                .forEach(System.out::println);
     }
 }
