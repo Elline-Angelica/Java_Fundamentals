80663524f67c6237af6982b994ebd06e4ab9161b
Apple <ellinemedrano1408@hotmail.com>
2021-01-03 18:22:02 +0100

    Updated

--- src/be/intecbrussel/JFUND_1/DiceGenerator.java
+++ src/be/intecbrussel/JFUND_1/DiceGenerator.java
@@ -1,6 +1,5 @@
 package be.intecbrussel.JFUND_1;
 
-import javax.sound.midi.Soundbank;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
@@ -12,27 +11,25 @@ public class DiceGenerator {
         Random random = new Random();
 
         System.out.println("Enter amount of throws");
-        int input = sc.nextInt();
+        int input;
 
-        while(input<20){
-            System.out.println("Enter amount 20 or more");
-        }
+        do {
+            input = sc.nextInt();
+        } while (input < 20);
 
-        //if (input < 20) { input = 20; }
-        //System.out.println("Selected amount of throws: " + input);
-
-        int result;
-        for (int i = 0; i < input; i++) {
-            result = random.nextInt(6);
-            result++;
-            System.out.println("Die result : " + result);
-        }
+        int offset = 1;
 
         int[] diceRolls = new int[6];
         Arrays.fill(diceRolls,0);
 
 
+        for (int i = input; i > 0; i--) {
+            diceRolls[random.nextInt(6)]++;
+        }
 
+        for (int i = 0; i < diceRolls.length; i++) {
+            System.out.printf("Amount of %d rolled: %d. Percentage: %.2f%s.%n", i + offset, diceRolls[i], diceRolls[i] * 100.0 / input, "%");
+        }
 
     }
 
