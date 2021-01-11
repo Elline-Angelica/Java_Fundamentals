4fb5f517db096f2c80e76c08be4273711df189ab
Apple <ellinemedrano1408@hotmail.com>
2021-01-11 18:53:46 +0100

    Updated

--- src/Chapter_21_FileIO_Foodshop/model/Register.java
+++ src/Chapter_21_FileIO_Foodshop/model/Register.java
@@ -1,5 +1,24 @@
 package Chapter_21_FileIO_Foodshop.model;
 
+
+import Chapter_21_FileIO_Foodshop.exception.NotEnoughMoneyInRegisterException;
+
 public class Register {
     private double money;
+
+    public double getMoney() {
+        return money = 0;
+    }
+
+    public void addMoney(double moneyToAdd) {
+        this.money += moneyToAdd;
+    }
+
+    public void deductMoney(double moneyToDeduct) throws NotEnoughMoneyInRegisterException {
+        if (moneyToDeduct > money) {
+            throw new NotEnoughMoneyInRegisterException("");
+        } else {
+            this.money -= moneyToDeduct;
+        }
+    }
 }
