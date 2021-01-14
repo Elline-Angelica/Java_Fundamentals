b16ec555afaa9ccd219cf6c7e68edf7a82491e2e
Apple <ellinemedrano1408@hotmail.com>
2021-01-14 15:58:54 +0100

    Updated

--- src/Chapter_21_FileIO/Excercises/FileIODemo/WriteData.java
+++ src/Chapter_21_FileIO/Excercises/FileIODemo/WriteData.java
@@ -1,12 +1,22 @@
 package Chapter_21_FileIO.Excercises.FileIODemo;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WriteData {
 
     public void writeStringToFile(String myText) {
 
-        File myTextFile = new File("");
+        File myTextFile = new File("resources/MyTextFile");
+
+        try {
+            FileWriter fileWriter = new FileWriter(myTextFile, true);
+            fileWriter.write(myText);
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
