3d48dd7c82811e71bf048b9b37649c7c07f57070
Apple <ellinemedrano1408@hotmail.com>
2021-01-26 11:29:55 +0100

    Updated

--- src/Chapter_24_Multithreading/Excercises/EXC9_P440/Calculator.java
+++ src/Chapter_24_Multithreading/Excercises/EXC9_P440/Calculator.java
@@ -1,4 +1,57 @@
 package Chapter_24_Multithreading.Excercises.EXC9_P440;
 
 public class Calculator {
+    private int value = 0;
+    private int result = 0;
+    private boolean done = true;
+    private boolean busy = false;
+    private Thread t;
+
+    public Calculator(){
+        Thread thread = new Thread(()-> calculate());
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+    public synchronized void setValue(int value){
+        while(busy){
+            try{
+                wait();
+            } catch (InterruptedException e){
+
+            }
+        }
+        this.value = value;
+        done = false;
+        busy = true;
+        notifyAll();
+    }
+
+    public synchronized int getResult(){
+
+        while(!done){
+            try{
+                wait();
+            } catch (InterruptedException e){
+            }
+        }
+        busy =false;
+        notifyAll();
+        System.out.println(Thread.currentThread().getName());
+        return result;
+    }
+
+    private synchronized void calculate() {
+        while(true){
+            while(done){
+                try{
+                    wait();
+                } catch (InterruptedException e){
+                }
+            }
+            result = value * value;
+            done = true;
+            notifyAll();
+        }
+    }
 }
